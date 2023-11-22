f815c6f3ef0fd19e6ebd3979abeb2422e3869075
derRaab <github.com@derRaab.com>
2023-11-22 14:40:25 +0100

    Download job not yet implemented.

--- src/job_download.php
+++ src/job_download.php
@@ -1,7 +1,11 @@
 <?php
 
-header('HTTP/1.0 200 OK');
-echo("You are authorized to do the download job.");
+// Constants
+include_once('constants.php');
+
+
+header('HTTP/1.0 501 Not Implemented');
+echo("You are authorized to do the download job but it's not yet implemented.");
 exit;
 
 ?>
